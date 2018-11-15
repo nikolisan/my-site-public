4b14f5295603429cb3d927a15092f4c4417fc4af
nikolisan <modaristis@gmail.com>
2018-11-15 14:23:12 +0200

    Add files via upload

---
+++ precache-manifest.fbad693f0805b0c0e47c964d9e324b7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "76814ab7ee0a0ed5cfa9136807a1029e",
+    "url": "/static/media/yosemite.76814ab7.jpg"
+  },
+  {
+    "revision": "06945f01a4afa9b77c7dcef9dd9ff842",
+    "url": "/static/media/computer.06945f01.jpg"
+  },
+  {
+    "revision": "6da91e78a3d691ad508514f57f4eac39",
+    "url": "/static/media/bridge.6da91e78.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39dea517d8a8d06c5ac1",
+    "url": "/static/js/main.39dea517.chunk.js"
+  },
+  {
+    "revision": "fde75fea0af03323e4ee",
+    "url": "/static/js/1.fde75fea.chunk.js"
+  },
+  {
+    "revision": "39dea517d8a8d06c5ac1",
+    "url": "/static/css/main.d86ec3fd.chunk.css"
+  },
+  {
+    "revision": "fde75fea0af03323e4ee",
+    "url": "/static/css/1.d70d8e9b.chunk.css"
+  },
+  {
+    "revision": "7699be1acc07d58c76099aa4ff100026",
+    "url": "/index.html"
+  }
+];
